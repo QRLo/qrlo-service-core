71de6c01a38ac6c4ad3c2c3943af7daf0357648e
rostradamus <rolee0429@gmail.com>
2021-04-21 21:04:48 -0400

    Init Project

---
+++ README.md
@@ -0,0 +1,8 @@
+# QRLo Core Service
+QRLo Backend Core Service
+
+## Usage
+## Contribution Guide
+## Supported features
+## Keywords
+
---
+++ src/main/java/com/qrlo/qrloservicecore/QrloServiceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.qrlo.qrloservicecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrloServiceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrloServiceCoreApplication.class, args);
+	}
+
+}
