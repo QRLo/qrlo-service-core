dc6c4543e6bd34b457c3eb65fb7e36eb1aaf87a4
rostradamus <rolee0429@gmail.com>
2021-04-27 23:28:37 -0400

    QRLO-12: TechDebt | Migrate to Spring 5.x with WebFlux

--- src/main/java/com/qrlo/qrloservicecore/user/model/User.java
+++ src/main/java/com/qrlo/qrloservicecore/user/model/User.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -13,6 +14,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -36,6 +38,8 @@ public class User implements UserDetails {
     private List<OAuth> oAuths;
     @JsonIgnore
     private List<Role> roles;
+    @CreatedDate
+    private LocalDateTime createAt;
 
     /**
      * Returns the authorities granted to the user. Cannot return <code>null</code>.
